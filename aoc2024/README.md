dfba1f36ddb7d5bcb508542be74a29a4f8699758
favilo <kevin.oberlies@elastic.co>
2024-11-30 22:05:46 -0800

    Day 1 done.
    I'm working on turning it into a template for later AoC years.

---
+++ aoc2024/README.md
@@ -0,0 +1,48 @@
+# Advent of Code 2024
+
+## Setup
+
+#### Notes for starting with new computer.
+
+This project uses [git-crypt](https://github.com/AGWA/git-crypt) to encrypt my secrets with gpg.
+If you don't have access to the private keys anymore, you can just generate a new `.env` file by copying the `dot-env.example` file.
+
+## Running days
+
+To run all days:
+
+```sh
+cargo run --release
+```
+
+Or to run a specific day:
+```sh
+cargo run --release -- -d 1
+```
+
+## Benchmarks
+
+Timings generated with:
+
+The `cargo-criterion` crate is useful to get nice benchmarks.
+
+```sh
+cargo criterion
+```
+
+Though not required, this just doesn't have as nice output, and will deprecate plots soon:
+
+```sh
+cargo bench
+```
+
+## Timings
+
+```
+day01/get_input         time:   [52.690 µs 53.011 µs 53.374 µs]
+day01/part1             time:   [12.893 µs 13.110 µs 13.386 µs]
+day01/part2             time:   [31.882 µs 32.071 µs 32.310 µs]
+
+```
+<details>
+</details>
---
+++ aoc2024/src/day01/mod.rs
@@ -0,0 +1,71 @@
+use std::iter::zip;
+
+use color_eyre::Result;
+use itertools::Itertools;
+use multiset::HashMultiSet;
+
+use crate::Runner;
+
+pub struct Day;
+
+impl Runner for Day {
+    type Input<'input> = (Vec<usize>, Vec<usize>);
+
+    fn day() -> usize {
+        1
+    }
+
+    fn get_input(input: &str) -> Result<Self::Input<'_>> {
+        Ok(input
+            .lines()
+            .map(|l| {
+                l.split("   ")
+                    .map(|n| n.parse::<usize>().unwrap())
+                    .next_tuple()
+                    .unwrap()
+            })
+            .collect::<(Vec<usize>, Vec<usize>)>())
+    }
+
+    fn part1(input: &Self::Input<'_>) -> Result<usize> {
+        let (mut a, mut b) = input.clone();
+
+        a.sort();
+        b.sort();
+
+        Ok(zip(a, b)
+            .map(|(a, b)| (a as isize - b as isize).unsigned_abs())
+            .sum::<usize>())
+    }
+
+    fn part2(input: &Self::Input<'_>) -> Result<usize> {
+        let (left, right): (_, HashMultiSet<usize>) = (
+            input.0.iter().cloned(),
+            HashMultiSet::from_iter(input.1.iter().cloned()),
+        );
+        Ok(left.map(|v| v * right.count_of(&v)).sum())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::helpers::{prod_case, sample_case};
+
+    sample_case! {
+        sample1 =>
+            input = "3   4\n\
+                4   3\n\
+                2   5\n\
+                1   3\n\
+                3   9\n\
+                3   3";
+            part1 = 11;
+            part2 = 31;
+    }
+
+    prod_case! {
+        part1 = 1681;
+        part2 = 201684;
+    }
+}
